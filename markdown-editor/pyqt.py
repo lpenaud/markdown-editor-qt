d3c96bf161c688761ed68e76547b06cd00fdcccf
lpenaud <l.penaud@zaclys.net>
2018-09-22 19:04:51 +0200

    Remove useless pyqt.py

--- markdown-editor/pyqt.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-from PyQt5 import QtCore, QtWidgets
-from PyQt5.Qt import QApplication, QClipboard
-from PyQt5.QtWidgets import QMainWindow, QWidget, QPlainTextEdit
-from PyQt5.QtCore import QSize
-from PyQt5.QtWebKitWidgets import QWebView
