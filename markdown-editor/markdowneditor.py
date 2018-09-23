58cc426075e78c71f52a5b572bccff710f601b04
lpenaud <l.penaud@zaclys.net>
2018-09-23 17:20:54 +0200

    Add onTextChanged signal to text_edit

--- markdown-editor/markdowneditor.py
+++ markdown-editor/markdowneditor.py
@@ -9,8 +9,13 @@ class MarkdownEditor(widgets.MainWindow):
     """docstring for MarkdownEditor."""
     def __init__(self):
         super(MarkdownEditor, self).__init__('Markdown Editor', 800, 400)
-        self.text_edit = widgets.TextEditor(self,0,0,400,400)
-        self.webview = widgets.WebView(self,401,0,400,400)
+        self._text_edit = widgets.TextEditor(self,0,0,400,400)
+        self._text_edit.textChanged.connect(self.onTextChanged)
+        self._webview = widgets.WebView(self,401,0,400,400)
+
+    def onTextChanged(self):
+        template = '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title>Hello world</title></head><body><pre>{}</pre></body></html>'
+        self._webview.html = template.format(self._text_edit.text_content)
 
 def main():
     app = QApplication(sys.argv)
