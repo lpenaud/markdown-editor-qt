690ad734e29d3e39b1a0225ae72ebe871c5fa1d6
lpenaud <l.penaud@zaclys.net>
2018-09-23 18:22:16 +0200

    Set layout of central widget

--- markdown-editor/markdowneditor.py
+++ markdown-editor/markdowneditor.py
@@ -3,19 +3,28 @@
 
 import sys
 import widgets
-from PyQt5.Qt import QApplication
+from PyQt5.Qt import QApplication, QBoxLayout
 
 class MarkdownEditor(widgets.MainWindow):
     """docstring for MarkdownEditor."""
     def __init__(self):
         super(MarkdownEditor, self).__init__('Markdown Editor', 800, 400)
-        self._text_edit = widgets.TextEditor(self,0,0,400,400)
-        self._text_edit.textChanged.connect(self.onTextChanged)
-        self._webview = widgets.WebView(self,401,0,400,400)
+        self._box = widgets.Box(QBoxLayout.LeftToRight)
+
+        self._text_editor = widgets.TextEditor()
+        self._text_editor.textChanged.connect(self.onTextChanged)
+
+        self._webview = widgets.WebView()
+        self.onTextChanged()
+
+        self._box.addWidget(self._text_editor)
+        self._box.addWidget(self._webview)
+
+        self.setCentralWidget(self._box)
 
     def onTextChanged(self):
         template = '<!DOCTYPE html><html lang="en" dir="ltr"><head><meta charset="utf-8"><title>Hello world</title></head><body><pre>{}</pre></body></html>'
-        self._webview.html = template.format(self._text_edit.text_content)
+        self._webview.html = template.format(self._text_editor.text_content)
 
 def main():
     app = QApplication(sys.argv)
