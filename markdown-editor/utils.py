239d20678ed277ffd99cda021f2df5809b7f93ff
lpenaud <l.penaud@zaclys.net>
2018-09-26 22:17:15 +0200

    Add path functions to utils

--- markdown-editor/utils.py
+++ markdown-editor/utils.py
@@ -2,5 +2,22 @@
 # -*- coding: utf-8 -*-
 
 from encodings.aliases import aliases
+from pathlib import Path
+import tempfile
 
 codes = set(aliases.values())
+
+def joinpath(root, *other):
+    return Path.joinpath(root, *other)
+
+def joinpath_to_cwd(*other):
+    return joinpath(Path.cwd(), *other)
+
+def joinpath_to_home(*other):
+    return joinpath(Path.home(), *other)
+
+def local_uri_to_path(uri):
+    return Path(uri[7:]) # len('file://') == 7
+
+def mktemp(suffix = '', prefix=tempfile.template, dir=None):
+    return Path(tempfile.mktemp(suffix, prefix, dir))
