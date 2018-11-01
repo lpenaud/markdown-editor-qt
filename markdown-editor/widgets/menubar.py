89391f80148dcb6ae2083b862f9f54576d5ebbcd
lpenaud <l.penaud@zaclys.net>
2018-11-01 20:32:06 +0100

    Add findMenu method to widgets.MenuBar

--- markdown-editor/widgets/menubar.py
+++ markdown-editor/widgets/menubar.py
@@ -7,43 +7,27 @@ from PyQt5.QtGui import QKeySequence
 
 class MenuBar(QMenuBar):
     """docstring for MenuBar."""
-    def __init__(self, parent = None):
-        super(MenuBar, self).__init__(parent)
-        self.menus = []
 
-    def appendMenu(self, title, iconName = None):
-        self.menus.append(QMenu(title, self))
-        iMenu = len(self.menus) - 1
-        if iconName:
-            self.menus[iMenu].setIcon(QIcon.fromTheme(iconName))
-        self.addMenu(self.menus[iMenu])
+    def addMenu(self, title):
+        menu = super(MenuBar, self).addMenu(title)
+        menu.setWindowTitle(title)
 
-    def findIndexMenu(self, title):
-        for i in range(0, len(self.menus)):
-            if self.menus[i].title() == title:
-                return i
-        return -1
+    def findMenu(self, title):
+        for menu in self.findChildren(QMenu):
+            if menu.windowTitle() == title:
+                return menu
 
     def addActionToMenu(self, menuTitle, action):
-        iMenu = self.findIndexMenu(menuTitle)
-        if iMenu == -1:
-            raise ValueError('Unknown menu')
-        self.menus[iMenu].addAction(action)
+        self.findMenu(menuTitle).addAction(action)
 
     def addActionsToMenu(self, menuTitle, actions):
-        iMenu = self.findIndexMenu(menuTitle)
-        if iMenu == -1:
-            raise ValueError('Unknown menu')
-        self.menus[iMenu].addActions(actions)
+        self.findMenu(menuTitle).addActions(actions)
 
     def addSeparatorToMenu(self, menuTitle):
-        iMenu = self.findIndexMenu(menuTitle)
-        if iMenu == -1:
-            raise ValueError('Unknown menu')
-        self.menus[iMenu].addSeparator()
+        self.findMenu(menuTitle).addSeparator()
 
-    def insertSeparatorToMenu(self, menuTitle, actionBefore):
-        iMenu = self.findIndexMenu(menuTitle)
-        if iMenu == -1:
-            raise ValueError('Unknown menu')
-        self.menus[iMenu].insertSeparator(actionBefore)
+    def insertSeparatorToMenu(self, menuTitle, actionBefore=None):
+        self.findMenu(menuTitle).insertSeparator(actionBefore)
+
+    def addSeparatorToMenu(self, menuTitle):
+        self.findMenu(menuTitle).addSeparator()
